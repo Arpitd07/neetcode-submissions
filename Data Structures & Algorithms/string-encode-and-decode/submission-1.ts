f7e97d487e4719f75cf09cfe8176b401b7cc42e8
Arpit Damani <132736420+Arpitd07@users.noreply.github.com>
2026-07-14 23:52:07 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.ts
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        let enc: string = "";
+        for (const word of strs) {
+            const encoded: string = word.length + "#" + word;
+            enc += encoded;
+        }
+        return enc;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const res: string[] = [];
+        let i: number = 0;
+        while (i < str.length) {
+            let j: number = str.indexOf("#", i);
+            let length: number = Number(str.substring(i, j));
+            let start: number = j + 1;
+            let end: number = start + length;
+            res.push(str.substring(start, end));
+            i = end;
+        }
+        return res
+    }
+}
